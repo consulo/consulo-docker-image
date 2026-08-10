b305426b6fc44c44f95cdf1d84c1c4029547554a
Valery Semenchuk <vistall.valeriy@gmail.com>
2026-08-10 10:13:18 +0300

    make flat

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-if [ ! -x "$CONSULO_DIR/Consulo/consulo.sh" ]; then
+if [ ! -x "$CONSULO_DIR/consulo.sh" ]; then
     echo "Downloading Consulo ($CONSULO_DIST_ID, channel $CONSULO_CHANNEL, version $CONSULO_VERSION)"
     mkdir -p "$CONSULO_DIR"
     curl -fsSL "https://api.consulo.io/repository/download?id=${CONSULO_DIST_ID}&channel=${CONSULO_CHANNEL}&platformVersion=${CONSULO_VERSION}" \
-        | tar -xz -C "$CONSULO_DIR"
+        | tar -xz -C "$CONSULO_DIR" --strip-components=1
 fi
 
-cd "$CONSULO_DIR/Consulo"
+cd "$CONSULO_DIR"
 exec ./consulo.sh "$@"
